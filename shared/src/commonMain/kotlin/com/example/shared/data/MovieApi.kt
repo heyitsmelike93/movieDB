6218ca38b52c50edc998bbdf26ba6a123190fc15
heyitsmelike93 <heyitsmelike93@gmail.com>
2026-04-07 01:42:38 +0300

    add moviedetail fun to interface

--- shared/src/commonMain/kotlin/com/example/shared/data/MovieApi.kt
+++ shared/src/commonMain/kotlin/com/example/shared/data/MovieApi.kt
@@ -1,7 +1,9 @@
 package com.example.shared.data
 
+import com.example.shared.data.remote.dto.MovieDetailDto
 import com.example.shared.data.remote.dto.MovieListResponseDto
 
 interface MovieApi {
     suspend fun getPopularMovies(page: Int): MovieListResponseDto
+    suspend fun getMovieDetail(movieId: Int): MovieDetailDto
 }
