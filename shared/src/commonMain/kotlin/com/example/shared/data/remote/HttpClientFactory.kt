f1145a3ab0489393ef8a90e60323840b4ca921c8
heyitsmelike93 <heyitsmelike93@gmail.com>
2026-04-06 22:47:38 +0300

    add httpclient with ktor

---
+++ shared/src/commonMain/kotlin/com/example/shared/data/remote/HttpClientFactory.kt
@@ -0,0 +1,27 @@
+package com.example.shared.data.remote
+
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.header
+import io.ktor.http.HttpHeaders
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+internal fun createHttpClient(): HttpClient = HttpClient {
+    install(ContentNegotiation) {
+        json(Json {
+            ignoreUnknownKeys = true
+            isLenient = true
+        })
+    }
+    install(Logging) {
+        level = LogLevel.HEADERS
+    }
+    defaultRequest {
+        url(ApiConfig.BASE_URL)
+        header(HttpHeaders.Authorization, "Bearer ${ApiConfig.READ_ACCESS_TOKEN}")
+    }
+}
